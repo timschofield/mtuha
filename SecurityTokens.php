d3e3d61bc3e4c7c943d2de23505980ae44a9bbeb
Tim Schofield <tim@weberpafrica.com>
2014-01-25 10:12:35 +0000

    Use css classes for edit and delete row links

--- SecurityTokens.php
+++ SecurityTokens.php
@@ -121,8 +121,8 @@ if (isset($_GET['Action']) and $_GET['Action'] == 'edit') {
 				<td><img src="' . $RootPath . '/css/' . $Theme . '/images/' . $myrow['tokenid'] . '.png" /></td>
 				<td>' . $myrow['tokenid'] . '</td>
 				<td>' . htmlspecialchars($myrow['tokenname'], ENT_QUOTES, 'UTF-8') . '</td>
-				<td><a href="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?SelectedToken=' . $myrow['tokenid'] . '&amp;Action=edit">' . _('Edit') . '</a></td>
-				<td><a href="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?SelectedToken=' . $myrow['tokenid'] . '&amp;Action=delete" onclick="return MakeConfirm(\'' . _('Are you sure you wish to delete this security token?') . '\', \'Confirm Delete\', this);">' . _('Delete') . '</a></td>
+				<td><a class="editlink" href="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?SelectedToken=' . $myrow['tokenid'] . '&amp;Action=edit">' . _('Edit') . '</a></td>
+				<td><a class="deletelink" href="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?SelectedToken=' . $myrow['tokenid'] . '&amp;Action=delete" onclick="return MakeConfirm(\'' . _('Are you sure you wish to delete this security token?') . '\', \'Confirm Delete\', this);">' . _('Delete') . '</a></td>
 			</tr>';
 	}
 
