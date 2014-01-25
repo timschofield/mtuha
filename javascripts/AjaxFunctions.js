79dea620a421061d46038ae94d7f755ee258a936
Tim Schofield <tim@weberpafrica.com>
2014-01-25 10:24:08 +0000

    Improvements to the way a form is submitted

--- javascripts/AjaxFunctions.js
+++ javascripts/AjaxFunctions.js
@@ -1,11 +1,15 @@
-function SubmitForm(FormName, Element) {
-	Target=FormName.action;
+function SubmitForm(FN, Element) {
+//function SubmitForm(FormName) {
+	FormName=document.getElementById(FN)
+	Target='includes/ProcessForm.php';
 	var PostData='';
 	for(var i=0,fLen=FormName.length;i<fLen;i++){
 		if(FormName.elements[i].type=='checkbox' && !FormName.elements[i].checked) {
 			FormName.elements[i].value=null;
 		}
-		PostData=PostData+FormName.elements[i].name+'='+FormName.elements[i].value+'&';
+		if(FormName.elements[i].name.substring(0,4)!='view'&&FormName.elements[i].name.substring(0,8)!='required') {
+			PostData=PostData+FormName.elements[i].name+'='+FormName.elements[i].value+'&';
+		}
 	}
 	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp=new XMLHttpRequest();
@@ -14,7 +18,7 @@ function SubmitForm(FormName, Element) {
 	}
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			document.getElementById(Elerment).innerHTML='<div id="SubWindow"><div id="contents">'+xmlhttp.responseText+'</div></div>';
+			document.getElementById(Element).innerHTML=xmlhttp.responseText;
 		}
 	}
 	xmlhttp.open("POST",Target,true);
